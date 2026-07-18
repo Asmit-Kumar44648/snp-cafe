5abf034e0cbaaeffba82089a9f8928cb9f64e1ad
Asmit-Kumar44648 <smith44648@gmail.com>
2026-07-18 18:02:44 +0530

    style: reduce logo sizes, fix dynamic menu image loading in production by moving assets to public, generate missing pasta doodle image

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -127,7 +127,7 @@ export default function Navbar() {
           {/* Center Logo */}
           <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-[60]">
             <Link to="/" onClick={handleHomeClick} className="nav-item flex flex-col items-center justify-center">
-              <SNPLogo className="w-16 h-16 md:w-20 md:h-20" />
+              <SNPLogo className="w-11 h-11 md:w-14 md:h-14" />
             </Link>
           </div>
 
